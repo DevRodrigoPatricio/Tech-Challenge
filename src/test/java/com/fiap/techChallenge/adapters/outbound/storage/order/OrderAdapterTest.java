95e782070702c276df0c62c401eee5c7c4ede014
leynerbueno <leyner_henrique@hotmail.com.br>
2025-05-15 15:34:56 -0300

    UPDATE: Order
    Adicionando classe de Teste OrderAdapterTest

---
+++ src/test/java/com/fiap/techChallenge/adapters/outbound/storage/order/OrderAdapterTest.java
@@ -0,0 +1,133 @@
+package com.fiap.techChallenge.adapters.outbound.storage.order;
+
+import com.fiap.techChallenge.domain.enums.Category;
+import com.fiap.techChallenge.domain.enums.ProductStatus;
+import com.fiap.techChallenge.domain.order.Order;
+import com.fiap.techChallenge.domain.order.OrderItem;
+import com.fiap.techChallenge.domain.order.OrderRepository;
+import com.fiap.techChallenge.domain.product.Product;
+import com.fiap.techChallenge.domain.product.ProductRepository;
+import com.fiap.techChallenge.utils.exceptions.WrongCategoryOrderException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class OrderAdapterTest {
+
+    private OrderRepository orderRepository;
+    private ProductRepository productRepository;
+    private OrderAdapter orderAdapter;
+
+    @BeforeEach
+    @SuppressWarnings("unused")
+    void setup() {
+        orderRepository = mock(OrderRepository.class);
+        productRepository = mock(ProductRepository.class);
+        orderAdapter = new OrderAdapter(orderRepository, productRepository);
+    }
+
+    @Test
+    void shouldAddItemSuccessfully() {
+        UUID orderId = UUID.randomUUID();
+        UUID productId = UUID.randomUUID();
+        String clientId = "ID_CLIENTE";
+        List<OrderItem> items = new ArrayList<>();
+
+        Product product = new Product(productId, "Lanche X", "X", new BigDecimal(10), Category.LANCHE, ProductStatus.DISPONIVEL, "img.jpg");
+        Order order = new Order(items, clientId, null, BigDecimal.ZERO, LocalDateTime.now());
+
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(order));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+        when(productRepository.listAvaiableCategorys()).thenReturn(List.of(Category.LANCHE));
+        when(orderRepository.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
+        Order result = orderAdapter.addItem(orderId, productId, 2);
+
+        assertEquals(1, result.getItems().size());
+        assertEquals(new BigDecimal(20), result.getPrice());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenAddItemWithWrongCategoryOrder() {
+        UUID orderId = UUID.randomUUID();
+        UUID productId = UUID.randomUUID();
+        String clientId = "ID_CLIENTE";
+        List<OrderItem> items = new ArrayList<>();
+
+        Product product = new Product(productId, "Batata Frita", "Batata", BigDecimal.TEN, Category.ACOMPANHAMENTO, ProductStatus.DISPONIVEL, "img.jpg");
+        Order order = new Order(items, clientId, null, BigDecimal.ZERO, LocalDateTime.now());
+
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(order));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+        when(productRepository.listAvaiableCategorys()).thenReturn(List.of(Category.LANCHE, Category.BEBIDA));
+
+        WrongCategoryOrderException ex = assertThrows(
+                WrongCategoryOrderException.class,
+                () -> orderAdapter.addItem(orderId, productId, 1)
+        );
+
+        String categoryList = Arrays.stream(Category.values())
+                .map(Enum::name)
+                .collect(Collectors.joining(", "));
+
+        assertTrue(ex.getMessage().equalsIgnoreCase("Os produtos devem ser selecionados na seguinte ordem: " + categoryList));
+    }
+
+    @Test
+    void shouldRemoveItemPartially() {
+        UUID orderId = UUID.randomUUID();
+        UUID productId = UUID.randomUUID();
+        String clientId = "ID_CLIENTE";
+        List<OrderItem> items = new ArrayList<>();
+
+        Product product = new Product(productId, "Batata Frita", "Batata", BigDecimal.valueOf(5), Category.ACOMPANHAMENTO, ProductStatus.DISPONIVEL, "img.jpg");
+
+        OrderItem item = new OrderItem(product, 3);
+        items.add(item);
+
+        Order order = new Order(items, clientId, null, BigDecimal.valueOf(15), LocalDateTime.now());
+
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(order));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+        when(orderRepository.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
+        Order result = orderAdapter.removeItem(orderId, productId, 2);
+
+        assertEquals(1, result.getItems().size());
+        assertEquals(1, result.getItems().get(0).getQuantity());
+        assertEquals(BigDecimal.valueOf(5), result.getPrice());
+    }
+
+    @Test
+    void shouldRemoveItemCompletely() {
+        UUID orderId = UUID.randomUUID();
+        UUID productId = UUID.randomUUID();
+        String clientId = "ID_CLIENTE";
+        List<OrderItem> items = new ArrayList<>();
+
+        Product product = new Product(productId, "Bebida Z", "Refrigerante", BigDecimal.valueOf(5), Category.BEBIDA, ProductStatus.DISPONIVEL, "img.jpg");
+
+        OrderItem item = new OrderItem(product, 2);
+        items.add(item);
+
+        Order order = new Order(items, clientId, null, BigDecimal.valueOf(10), LocalDateTime.now());
+
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(order));
+        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
+        when(orderRepository.save(any())).thenAnswer(invocation -> invocation.getArgument(0));
+
+        Order result = orderAdapter.removeItem(orderId, productId, 2);
+
+        assertTrue(result.getItems().isEmpty());
+        assertEquals(BigDecimal.ZERO, result.getPrice());
+    }
+}
